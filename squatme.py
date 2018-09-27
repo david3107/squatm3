414ba63528fd412b8fbb1c27d88e36e13dbce4f6
Stefan <stefan.petrushevski@secbase.nl>
2018-09-27 16:26:11 +0200

    refactoring and sigint handler

--- squatme.py
+++ squatme.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-import argparse
-import json
+import argparse, json, signal, sys
 import urllib.parse
 from modules.GoDaddyChecker import GoDaddy
 from modules.Remover.RemoveOneLetter import RemoveOneLetter
@@ -9,9 +8,8 @@ from modules.Substitutions.Flipper import Flipper
 from modules.Tldmodule.TldSelector import TldSelector
 from modules.Urlchecker import checkvalidity
 
-
-def main():
-    print("""
+def banner():
+        print("""
  ___             __    _           ____
 / __> ___  _ _  /. | _| |_ ._ _ _ <__ /
 \__ \/ . || | |/_  .| | |  | ' ' | <_ \\
@@ -19,6 +17,12 @@ def main():
        | |
 \n""")
 
+def signal_handler(sig, frame):
+        sys.exit(0)
+
+def prepare_arguments():
+    global args, url, tld, available, homoglyph, flipper, remove, all_args
+    
     parser = argparse.ArgumentParser(description='SquatMe v1.0 - Copyright @david3107')
     parser.add_argument('--url', dest='url', help='url to be squatted')
     parser.add_argument('--tld', dest='tld', type=bool, nargs='?',
@@ -39,7 +43,6 @@ def main():
     parser.add_argument('--available', dest='available', type=bool, nargs='?',
                         const=True, default=False,
                         help='lists only the available domains')
-
     args = parser.parse_args()
     url = args.url
     tld = args.tld
@@ -49,44 +52,44 @@ def main():
     remove = args.remove
     #output = args.output
     #output_format = args.output_format
-    all = args.all
-    domains = []
+    all_args = args.all
 
-
-    if url is None:
-        print("--url field is mandatory")
-        return
-
-    if not checkvalidity.check_valid_url(url):
-        print("URL not valid. Exiting ...")
-        return
-
-    
-    print("URL " + url + " is valid: let's squat\n")
-
+def prepare_list_domains_based_on_input():
     '''
     create the list of domains based on the attacks that were selected
     '''
-    if remove or all:
+    domains = []
+    if remove or all_args:
         print("\n\x1b[6;30;42m" + "[*]Letter removal" + '\x1b[0m')
         step1 = RemoveOneLetter(url)
         domains = step1.remove_letters()
 
-    if homoglyph or all:
+    if homoglyph or all_args:
         print("\x1b[6;30;42m[*] Homoglyph attack" + '\x1b[0m')
         step2 = HomoglyphAttack(url)
         step2.load_letters()
         domains = domains + step2.switch_letters()
 
-    if flipper or all:
+    if flipper or all_args:
         print("\x1b[6;30;42m[*] Flipper attack" + '\x1b[0m')
         step3 = Flipper(url)
         domains = domains + step3.flip_letters()
 
-    if len(domains) == 0:
-        print("Exit: No domains have been generated!!")
-        return
+    return domains
 
+
+def check_required_params():
+    global url
+    if url is None:
+        print("--url field is mandatory")
+        exit(0)
+
+    if not checkvalidity.check_valid_url(url):
+        print("URL not valid. Exiting ...")
+        exit(0)
+
+def check_domain_availability(domains):
+    global tld, url
     if tld is False:
         tlds = ['com']
     else:
@@ -116,10 +119,26 @@ def main():
                     print('Domain ' + urllib.parse.unquote(complete_domain) + ' : No info retrieved, try manually ')
                 combined_domain_list.append(complete_domain)
         except Exception as e:
-            print(e)
+            print(str(e) + str("####")) 
             pass
+
+
+
+def main():
+    domains = []
+    banner()
+    prepare_arguments()
+    check_required_params()
+    domains = prepare_list_domains_based_on_input()
+
+    if len(domains) == 0:
+        print("Exit: No domains have been generated!!")
+        return
+    check_domain_availability(domains)
     
 
 
 if __name__ == "__main__":
+    args = url = tld = available = homoglyph = flipper = remove = all_args = None
+    signal.signal(signal.SIGINT, signal_handler)
     main()
