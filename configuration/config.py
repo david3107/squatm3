448a582c0d8f7a89ac69279ee2ea9988a158283e
Stefan Petrushevski <stefan.petrushevski@ing.nl>
2018-10-25 14:46:40 +0200

    refactoring - added configuration

---
+++ configuration/config.py
@@ -0,0 +1,16 @@
+DB = {
+    'homoglyph': 'db/homoglyph',
+    'top_domains': 'db/top_domains',
+    'user': 'user',
+    'password': 'password',
+    'port': 3306
+}
+
+
+GODADDY = {
+	'key': '',
+	'secret': '',
+	'api_endpoints': ['/v1/domains/tlds','/v1/domains/available'],
+	'api_url': 'https://api.godaddy.com/',
+	'url': 'https://uk.godaddy.com/domainsapi/v1/search/exact?q='
+}
